0806a0ba8e479ea54e37b96e4d3164fc2edc5b51
Krish Goyal <goyalkrish183@gmail.com>
2025-07-21 13:47:27 +0530

    fixing porting

--- app.py
+++ app.py
@@ -94,7 +94,9 @@ if sys.stdin.isatty():
 import uvicorn
 
 if __name__ == "__main__":
-    config = uvicorn.Config(app=app, host="127.0.0.1", port=8000, log_level="info")
+    port = int(os.environ.get("PORT", 8000))
+    config = uvicorn.Config(app=app, host="0.0.0.0", port=port, log_level="info")
+
     server = uvicorn.Server(config)
 
     async def run_with_shutdown():
