d064c21b5d4446834e010501ad824d184ce28ff3
Krish Goyal <goyalkrish183@gmail.com>
2025-07-21 14:17:33 +0530

    railway

--- app.py
+++ app.py
@@ -97,6 +97,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     config = uvicorn.Config(app=app, host="0.0.0.0", port=port, log_level="info")
 
+
     server = uvicorn.Server(config)
 
     async def run_with_shutdown():
